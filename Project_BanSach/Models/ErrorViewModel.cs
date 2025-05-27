bac199e6db3d945363477cdca8fb9c46d20f622b
nhat-trieu <nhattrieuisme@gmail.com>
2025-05-27 09:09:12 +0700

    Delete Project_BanSach directory

--- Project_BanSach/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Project_BanSach.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
